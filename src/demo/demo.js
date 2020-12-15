cd78f63ce5b64f64cf348b61acd5915f29a127a1
rsilivestr <rsilivestr@gmail.com>
2020-12-15 10:44:26 +0300

    Clean src/demo/demo.js

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -1,14 +1,6 @@
-const opt1 = { handlerCount: 2 };
+const s1o = { handlerCount: 2 };
 
-const sl1 = RSlider.create('#foo', opt1);
-// const pan1 = sl1.addPanel();
-
-// const sl2 = RSlider.create('#bar', { isHorizontal: false });
-// const pan2 = sl2.addPanel();
-
-// const sl3 = RSlider.create('#fizz', { stepSize: 1 });
-// const sc3 = sl3.addScale();
-// const pan3 = sl3.addPanel();
+const s1 = RSlider.create('#foo', s1o);
 
 const opt4 = {
   minValue: 0,
@@ -18,6 +10,3 @@ const opt4 = {
   isHorizontal: false,
   showTooltip: false,
 };
-
-// const sl4 = RSlider.create('#buzz', opt4);
-// const pan4 = sl4.addPanel();
